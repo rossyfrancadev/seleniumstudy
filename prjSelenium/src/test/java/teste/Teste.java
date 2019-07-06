efc75702169de8d2421807abd89fc41b277dfd4e
Rossywan Correa de Franca <rossyfrancadev@gmail.com>
2019-07-06 16:37:49 -0300

    project refactor

---
+++ prjSelenium/src/test/java/teste/Teste.java
@@ -0,0 +1,37 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Teste {
+	static WebDriver driver;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	
+		System.setProperty("webdriver.chrome.driver", "Y:/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().fullscreen();
+		driver.get("https://www.youtube.com");
+	}
+
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+
+	}
+	
+
+}
